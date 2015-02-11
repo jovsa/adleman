89a14fc6039c9f11447ff8d848a1c1214a3750d3
Jovan Sardinha <jovan.sardinha@gmail.com>
2015-02-11 00:25:30 -0500

    initial work on the initial exploration

---
+++ config/global_config.R
@@ -0,0 +1,21 @@
+# Working directory initialization
+wd <- "./Documents/GitHub/Project_Adleman"
+setwd(wd)
+
+library(rattle)
+library(tm)
+library(SnowballC)
+library(caret)
+library(plyr)
+library(tau)
+library(ngram)
+library(knitr)
+library(Matrix)
+library(RWeka)
+library(ggplot2)
+library(reshape)
+library(wordcloud)
+
+
+# Cleaners
+wd <- NULL
