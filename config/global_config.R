dc723c5ebdfdf324e76efef7cab65946577b60fb
JovanSardinha <jovan.sardinha@gmail.com>
2015-02-23 23:49:07 +0000

    changes to the subset extraction

--- config/global_config.R
+++ config/global_config.R
@@ -1,7 +1,26 @@
-# Working directory initialization
-wd <- "./Documents/GitHub/Project_Adleman"
+# Working directory initialization - on EC2
+
+#wd <- "./Documents/GitHub/Project_Adleman"
+wd <- "./GitHub/Project_Adleman"
 setwd(wd)
 
+# Package Installation
+# install.packages("rattle")
+# install.packages("tm")
+# install.packages("SnowballC")
+# install.packages("caret")
+# install.packages("plyr")
+# install.packages("tau")
+# install.packages("ngram")
+# install.packages("knitr")
+# install.packages("Matrix")
+# install.packages("RWeka")
+# install.packages("ggplot2")
+# install.packages("reshape")
+# install.packages("wordcloud")
+
+
+# Library initialization
 library(rattle)
 library(tm)
 library(SnowballC)
